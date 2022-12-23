38c5e3517254bf347ecee2dff529feefad881826
Josiel Rocha <josielrochads@gmail.com>
2022-12-22 21:06:02 -0300

    Módulo 6. Introdução Algoritmos. Realização de um algoritmo simples para o exercício do módulo 6.

---
+++ src/Mod6_Algoritmo/Mod6_Algoritmo.java
@@ -0,0 +1,15 @@
+package Mod6_Algoritmo;
+
+// Algoritmo relacionado a levantar da cama.
+public class Mod6_Algoritmo {
+    public static void main(String[]args)
+    {System.out.println("O que faço quando acordo:");
+    System.out.println("1 - Retiro a coberta e desligo o despertador.");
+    System.out.println("2 - Me levanto e calço a minha chinela.");
+    System.out.println("3 - Vou ao banheiro para realizar minha higiene pessoal.");
+    System.out.println("4 - Após realizar a higiene bucal tomo banho.");
+    System.out.println("5 - Me visto e depois tomo café.");
+    System.out.println("6 - Normalmente é isso que acontece na minha rotina. Fim.");
+    }
+
+}
